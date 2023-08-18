5c3cae37e16d49cd8fb4bd808b4d2eb4ffe82cdf
Kristian Dotson <KDotson387@gmail.com>
2023-08-18 11:58:24 -0500

    added drone

--- mainMove.py
+++ mainMove.py
@@ -6,15 +6,25 @@ import time
 # line 7 creates an instance of the tello class and creates the connection to the drone 
 tello = Tello.Tello() 
 
+# initalize connection
 tello.init()
 
 
 
 # takeoff_and_land() that is responsible for taking off the drone, keeping it in the air for a few seconds, and then landing it
-def takeoff_and_land():
+# def takeoff_and_land():
+#     response = tello.takeoff()
+#     print("Takeoff response:", response)
+#     time.sleep(120)  # Keeping the drone in the air for 120 seconds(2 min)
+#     response = tello.land()
+#     print("Land response:", response)
+
+def take_off():
     response = tello.takeoff()
     print("Takeoff response:", response)
-    time.sleep(120)  # Keeping the drone in the air for 120 seconds(2 min)
+
+# Function to land the drone
+def land():
     response = tello.land()
     print("Land response:", response)
 
@@ -54,17 +64,18 @@ def move_backward():
 
 def main():
     print("Press the following keys to control the drone:")
-    print("  - 't' to take off and land")
+    print("  - 't' to take off")
     print("  - 'u' to move up")
     print("  - 'f' to move forward")
     print("  - 'b' to move backward")
     print("  - 'r' to rotate clockwise")
     print("  - 'p' to perform a flip")
+    print("press L to land")
     print("  - 'q' to quit")
 
     while True:
         if keyboard.is_pressed('t'):
-            takeoff_and_land()
+            take_off()
         elif keyboard.is_pressed('u'):
             move_up()
         elif keyboard.is_pressed('f'):
@@ -75,6 +86,8 @@ def main():
             rotate_clockwise()
         elif keyboard.is_pressed('p'):
             flip()
+        elif keyboard.is_pressed('L'):
+            land()    
         elif keyboard.is_pressed('q'):
             break
 
