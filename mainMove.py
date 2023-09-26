2fdb1dd22b94474f3adebe60ccc4a97bc71ff656
Kristian Dotson <KDotson387@gmail.com>
2023-09-26 15:07:18 -0500

    basic move work

--- mainMove.py
+++ mainMove.py
@@ -3,63 +3,67 @@ import keyboard
 import time
 
 # Initialize the Tello object
-# line 7 creates an instance of the tello class and creates the connection to the drone 
 tello = Tello.Tello() 
 
-# initalizing the coqnnection
+# Initialize the connection
 tello.init()
 
-
-
-# takeoff_and_land() that is responsible for taking off the drone, keeping it in the air for a few seconds, and then landing it
-# def takeoff_and_land():
-#     response = tello.takeoff()
-#     print("Takeoff response:", response)
-#     response = tello.land()
-#     print("Land response:", response)
-
+# Function to take off
 def take_off():
     response = tello.takeoff()
     print("Takeoff response:", response)
 
+# Function to hover
 def hover():
     response = tello.hover()
-    print("hover response:", response)
+    print("Hover response:", response)
 
 # Function to land the drone
 def land():
     response = tello.land()
     print("Land response:", response)
 
+# Function to move up
 def move_up():
     response = tello.up(50)  
     print("Up response:", response)
 
+# Function to move down
 def move_down():
     response = tello.down(50)
     print("Down response:", response)
-    
 
+# Function to move forward
 def move_forward():
     response = tello.forward(100)  
     print("Forward response:", response)
 
+# Function to move backward
+def move_backward():
+    response = tello.back(100)
+    print("Backward response:", response)
+
+# Function to turn left
 def move_left():
     response = tello.left(100)  
     print("Left response:", response)
 
+# Function to turn right
 def move_right():
-    response = tello.right(100)  # Replacing the 50 with the new distance in cm
-    print("Right response:", response)    
+    response = tello.right(100)
+    print("Right response:", response)
 
+# Function to rotate clockwise
 def rotate_clockwise():
-    response = tello.rotate('cw', 90)  # this is Rotating the drone 90 degrees clockwise
+    response = tello.rotate('cw', 90)
     print("Rotate response:", response)
 
+# Function to perform a flip
 def flip():
     response = tello.flip('f')  
     print("Flip response:", response)
 
+# Function to circle around an object
 def circle_around_object():
     for _ in range(4):
         response = tello.forward(50)  # Move forward
@@ -67,6 +71,7 @@ def circle_around_object():
         response = tello.rotate('cw', 90)  # Rotate 90 degrees clockwise
         print("Rotate response:", response)
 
+# Function to fly in a figure-eight pattern
 def figure_eight():
     for _ in range(2):
         # Loop to complete one loop of the figure-eight
@@ -79,29 +84,22 @@ def figure_eight():
         response = tello.rotate('ccw', 90)  # Rotate 90 degrees counterclockwise
         print("Rotate response:", response)
 
-
-
-def move_backward():
-    response = tello.back(100)
-    print("Backward response:", response)
-
-
-
+# Main control loop
 def main():
     print("Press the keys to control the drone:")
     print("  - 't' to take off")
-    print(" - 'h' to move hover")
+    print("  - 'h' to hover")
     print("  - 'i' to move up")
-    print(" - 'k' to move down")
+    print("  - 'k' to move down")
     print("  - 'w' to move forward")
     print("  - 's' to move backward")
-    print(" - 'a' to turn left")
-    print(" - 'd' to turn right")
+    print("  - 'a' to turn left")
+    print("  - 'd' to turn right")
     print("  - 'r' to rotate clockwise")
     print("  - 'f' to perform a flip")
     print("  - 'c' to circle around an object")
     print("  - 'e' to fly in a figure-eight pattern")
-    print("  - press L to land")
+    print("  - 'L' to land")
     print("  - 'q' to quit")
 
     while True:
@@ -114,9 +112,9 @@ def main():
         elif keyboard.is_pressed('k'):
             move_down()
         elif keyboard.is_pressed('a'):
-            move_left    
+            move_left()  
         elif keyboard.is_pressed('d'):
-            move_right    
+            move_right()  
         elif keyboard.is_pressed('w'):
             move_forward()
         elif keyboard.is_pressed('s'):  
